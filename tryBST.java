06b6edb1fd09933930e87272d4e90e3b84f292e5
Maramba0809 <4447141@myuwc.ac.za>
2026-03-25 14:53:11 +0200

    insert function called and main tryBST function checked

--- tryBST.java
+++ tryBST.java
@@ -32,5 +32,15 @@ public class tryBST {
         }
 
         return root;
-    }
-}
+    }public void insert (int key){
+            root =insertRec(root,key);
+}// tryBST checked properly
+    boolean istryBST (tNode node ,int min,int max){
+        if (node==null){
+            return true;
+
+        }if (node.key <min || node.key > max){
+            return false;
+
+        }return istryBST(node.left,min,node.key -1)&& istryBST(node.right ,node.key +1,max);
+
