2e93b1a5aee70cd9cf7e629b251f2c3eff31d306
Maramba0809 <4447141@myuwc.ac.za>
2026-03-25 14:34:22 +0200

    function defined

--- tryBST.java
+++ tryBST.java
@@ -1,1 +1,14 @@
 //java file created 
+import java.util.*;
+
+class tNode{
+    int key;
+    tNode left,right;
+    
+    public tNode(int key){
+        this.key=key;
+        left=null;
+        right=null;
+        
+    }
+} 
